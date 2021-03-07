3c3403c81efa003b7c125bdee92f4f5026ccdead
luiswislo <luisfwilson1998@gmail.com>
2021-03-07 16:33:34 -0700

    vue2 and buefy

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
-<p>Hello, InkChat!</p>
+    <b-button>heello</b-button>
 </template>
+
 
 <script>
 export default {
@@ -10,3 +11,21 @@ export default {
   }
 }
 </script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
