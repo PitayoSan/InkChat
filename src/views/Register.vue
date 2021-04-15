39b60afdbd901ee12602185c35eb49f004327114
luiswislo <luisfwilson1998@gmail.com>
2021-04-14 17:52:54 -0700

    register template

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,5 +1,27 @@
 <template>
-    <p>Register</p>
+    <main class="columns is-justify-content-center almost-v-centered">
+        <section class="column is-12-mobile is-5-tablet is-5-desktop is-3-fullhd border">
+            <div class="has-text-centered">
+                <h1 class="title">Register</h1>
+            </div>
+            <br><br><br>
+            <b-field label="Email">
+                <b-input type="email"></b-input>
+            </b-field>
+            <b-field label="Username">
+                <b-input type="text"></b-input>
+            </b-field>
+            <br><br>
+            <b-field label="Password">
+                <b-input type="password"></b-input>
+            </b-field>
+            <b-field label="Confirm Password">
+                <b-input type="password"></b-input>
+            </b-field>
+            <br><br>
+            <b-button expanded tag="router-link" to="/">Create Account</b-button>
+        </section>
+    </main>
 </template>
 
 <script>
